15cbf1e2e1c792743c5875719e4400d4b1e3a8c8
S4nches <luis-lipe@hotmail.com>
2021-03-28 20:07:26 -0300

    changing port

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const routes = require("./routes.js");
 
-const port = process.env.PORT || 3000;
+const port = 80;
 
 app.use(express.json());
 app.use("/", routes);
